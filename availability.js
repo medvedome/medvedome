ed7bc63e57533e694d2ade5e01731cb8a1073285
medvedome <gergelyb@tcd.ie>
2026-05-30 13:17:12 +0300

    Update availability.js

--- availability.js
+++ availability.js
@@ -1,14 +1,8 @@
 // Edit this small file from GitHub when you need to mark nights as booked.
 // Use the format YYYY-MM-DD and keep each date inside quotes.
 window.medveBookedDates = [
-  "2026-06-07",
-  "2026-06-08",
-  "2026-06-14",
-  "2026-06-15",
   "2026-07-11",
   "2026-07-18",
   "2026-07-19",
   "2026-07-20",
-  "2026-08-02",
-  "2026-08-03"
 ];
